126ab2e9b1c4b87e05afc64234754067b5f7889d
oisho <oishogames@gmail.com>
2025-02-22 13:46:18 +0700

    ver. 22022025
    added shader material (2d tree shake with Vertex Shader effects)

--- scenes/components/hurt_component.gd
+++ scenes/components/hurt_component.gd
@@ -7,6 +7,7 @@ signal hurt
 
 func _on_area_entered(area: Area2D) -> void:
 	var hit_component = area as HitComponent
+	print(hit_component.get_parent().name, " damaged ", get_parent().name)
 
 	if tool == hit_component.current_tool:
 		hurt.emit(hit_component.hit_damage)
